9565460f0d9d0cebcefe99d86a0789ea799b570f
r1d1 <r1d1@users.noreply.github.com>
2015-07-03 21:04:09 +0200

    Some tests on the plot script

--- misc/nexuiz_plot.py
+++ misc/nexuiz_plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,6 +9,19 @@ dataFile = 'processed_time_nex.txt'
 #dataToPlot=open(dataFile)
 #timeValuesStr = dataToPlot.read()
 #dataToPlot.close()
+
+# MAtplotlib general parameters :
+params = {
+	'axes.labelsize': 8,
+	'text.fontsize': 8,
+	'legend.fontsize': 10,
+	'xtick.labelsize': 10,
+	'ytick.labelsize': 10,
+	'text.usetex': False,
+	'figure.figsize': [6.0, 6.0]
+}
+mpl.rcParams.update(params)
+
 
 timeValues = np.loadtxt(dataFile)
 
@@ -18,12 +32,34 @@ xTicksLabels=['Game '+str(n) for n in np.arange(len(timeValues))]
 fig, ax1 = plt.subplots()
 ax2=plt.twinx()
 
-ax1.plot(x, timeValues /60.0, 'b')
-ax2.plot(x, np.cumsum(timeValues/60.0), 'r')
+#plt.axes(frameon=0)
+#plt.grid()
+
+#plt.plot(x, timeValues /60.0, linewidth=2, color='b')
+#plt.plot(x, np.cumsum(timeValues/60.0), linewidth=2, color='r')
+plt.xticks(np.arange(len(timeValues)), xTicksLabels, rotation='vertical')
+ax1.plot(x, timeValues /60.0, linewidth=2, color='b')
+ax2.plot(x, np.cumsum(timeValues/60.0), linewidth=2, color='r')
 ax1.set_xlabel('Games played')
-#plt.xticks(np.arange(len(timeValues)), xTicksLabels, rotation='vertical')
+ax1.set_xlabel('Games played')
 ax1.set_ylabel('Time (min)', color='b')
 ax2.set_ylabel('Cumulative time', color='r')
+#plt.ylabel('Time (min)', color='b')
+#plt.set_ylabel('Cumulative time', color='r')
+leg = plt.legend(["Game duration", "Total time"], loc=2)
+#leg1 = plt.legend(["Game duration"], loc=2)
+#leg2 = plt.legend(["Total time"], loc=1)
 
+#frame=leg.get_frame()
+#frame.set_facecolor('0.9')
+#frame.set_edgecolor('0.9')
+#frame1=leg1.get_frame()
+#frame2=leg2.get_frame()
+#frame1.set_facecolor('0.9')
+#frame1.set_edgecolor('0.9')
+#frame2.set_facecolor('0.9')
+#frame2.set_edgecolor('0.9')
 
 plt.show()
+
+#fig.savefig('durationsNexuiz.png')
