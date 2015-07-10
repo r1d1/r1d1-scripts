470fde94028ceef9af74217da8507b2ed3cf08e2
Erwan Renaudo <r1d1@users.noreply.github.com>
2015-07-10 14:48:41 +0200

    Very basic command communication food market style

--- network/server.py
+++ network/server.py
@@ -13,7 +13,7 @@ from serverFunctions import *
 # ====================================================================================
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12346                # Reserve a port for your service.
+port = 12345                # Reserve a port for your service.
 
 # Trying to bind the socket :
 try:
@@ -30,12 +30,20 @@ print "Socket listening ... ", host
 
 nbOfConnections=0;
 
+productList=["banana", "apple", "grape", "apricot", "mutton"]
+basePrice = 3.5
+priceList= np.trunc(100*(basePrice+np.random.randn(len(productList))))/100.0
+#priceList= 2.0+np.around(np.random.randn(len(productList)), decimals=2)
+priceList = priceList.tolist()
+
+print productList, priceList
+
 while True:
 	c, addr = s.accept()     # Establish connection with client.
 	nbOfConnections += 1;
 	print 'Got connection from', addr, ' (', nbOfConnections,' people connected)'
 	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-	start_new_thread(clientThread ,(c,))
+	start_new_thread(clientThread ,(c, productList, priceList))
 	#c.send('Thank you for connecting '+str(np.random.rand()))
 	#c.close()                # Close the connection
 
--- network/serverFunctions.py
+++ network/serverFunctions.py
@@ -10,16 +10,57 @@ from thread import *
 import numpy as np
 # ====================================================================================
 #Function for handling connections. This will be used to create threads
-def clientThread(conn):
+def clientThread(conn, prod, price):
 	#Sending message to connected client
 	conn.send('Welcome to the server. Type something and hit enter\n')
+	myMoney = 100
+	myProducts = np.zeros(len(prod)).tolist()
 	#infinite loop so that function do not terminate and thread do not end.
 	while True:
 		#Receiving from client
 		data = conn.recv(1024)
-		reply = 'OK...' + data
 		if not data: 
 			break
+		
+		command=data.split()
+		reply = "OK... "
+
+		if command[0] == "/quit":
+			break
+		elif command[0] == "/money":
+			print "money:", myMoney
+		elif command[0] == "/buy":
+			print "buy", command[1], command[2]
+			if (command[1] in prod):
+				try:
+					productNumber=int(command[2])
+				except IndexError, ValueError:
+					print "Quantity is Not a number, try again !"
+				productIndex=prod.index(command[1])
+				totalPrice = productNumber * price[productIndex]
+				print productIndex, totalPrice
+				if totalPrice <= myMoney:
+					myMoney = myMoney - totalPrice 
+					myProducts[productIndex] = myProducts[productIndex] + productNumber
+					reply = "Transaction done !"
+				else:
+					reply= "Not enough money !"
+			else:
+				reply = "Unknown product !"
+				print reply 
+		elif command[0] == "/sell":
+			print "sell", data[6:]
+		elif command[0] == "/product":
+			# /product <name> <>
+			print "product", data[9:]
+		elif command[0] == "/status":
+			products=[str(yourp[0])+" "+str(yourp[1]) for yourp in zip(*[prod, myProducts])]
+			status=str("Your account currently has " + str(myMoney) + " unit, you owe " + str(products))
+			reply = reply + status
+			print status
+		else:
+			pass
+		reply = reply + "\n"
 		conn.sendall(reply)
 	#came out of loop
 	conn.close()
